1a8d13a68e17842ffbea02ab7d2da448ba3b132e
Shenya De Silva <shenya44@outlook.com>
2024-06-21 23:26:09 +0200

    Beautify the main function

--- cpp01_memory_allocation/ex02/main.cpp
+++ cpp01_memory_allocation/ex02/main.cpp
@@ -7,11 +7,20 @@ int main(void)
 	std::string *stringPTR = &brain;
 	std::string &stringREF = brain;
 
-	std::cout << "Memory address of brain: ";
+	std::cout << "Memory address of brain: \t";
 	std::cout << &brain << std::endl;
-	std::cout << "Memory address of stringPTR: ";
+	std::cout << "Memory address of stringPTR: \t";
 	std::cout << stringPTR << std::endl;
-	std::cout << "Memory address of stringREF: ";
+	std::cout << "Memory address of stringREF: \t";
 	std::cout << &stringREF << std::endl;
+
+	// values
+	std::cout << "Value of brain: \t\t";
+	std::cout << brain << std::endl;
+	std::cout << "Value of stringPTR: \t\t";
+	std::cout << *stringPTR << std::endl;
+	std::cout << "Value of stringREF: \t\t";
+	std::cout << stringREF << std::endl;
+
 	return (0);
 }
