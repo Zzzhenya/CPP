5704c6f970d2e4f8e98cdea7b4cb24ca937ede01
Shenya De Silva <shenya44@outlook.com>
2024-07-15 14:07:03 +0200

    Initial commit

---
+++ cpp03_inheritance/ex01/ScavTrap.cpp
@@ -0,0 +1,11 @@
+#include "ScavTrap.hpp"
+
+ScavTrap::ScavTrap(void)
+{
+	std::cout << "Scav constructor\n";
+}
+
+ScavTrap::~ScavTrap(void)
+{
+	std::cout << "Scav destructor\n";
+}
---
+++ cpp03_inheritance/ex01/main.cpp
@@ -0,0 +1,27 @@
+#include "ClapTrap.hpp"
+#include "ScavTrap.hpp"
+
+int main (void)
+{
+	// ClapTrap b("One");
+	// ClapTrap a("Two");
+	ScavTrap x;
+
+	x.print_score();
+	// a.print_score();
+	// b.print_score();
+	
+	// for(int i = 0; i < 20; i++)
+	// {
+	// 	if (!(i%4))
+	// 		b.beRepaired(i);
+	// 	else
+	// 	{
+	// 		b.attack("Two");
+	// 		a.takeDamage(1);// b.getDamageCount()
+	// 	}
+	// 	a.print_score();
+	// 	b.print_score();
+	// }
+	return (0);
+}
