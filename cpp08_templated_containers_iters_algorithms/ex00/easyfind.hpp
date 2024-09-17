08a1197918ae4cfd908ce84ea7497d501618b297
Shenya De Silva <shenya44@outlook.com>
2024-09-17 17:01:07 +0200

    Replaced with find() from algorithms

--- cpp08_templated_containers_iters_algorithms/ex00/easyfind.hpp
+++ cpp08_templated_containers_iters_algorithms/ex00/easyfind.hpp
@@ -8,12 +8,7 @@
 template <typename T>
 typename T::iterator easyfind(T &haystack, int needle)
 {
-	for (typename T::iterator it = haystack.begin(); it != haystack.end(); it++)
-	{
-		if (*it == needle)
-			return (it);
-	}
-	return (haystack.end());
+	return (find(haystack.begin(), haystack.end(), needle));
 }
 
 #endif
