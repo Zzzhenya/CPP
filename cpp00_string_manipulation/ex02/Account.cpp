e6dc5b477e88b2ffc496b14fdbe4aa3898f29436
Shenya De Silva <sde-silv@c4c3c4.42berlin.de>
2024-11-04 16:57:07 +0100

    Bonus: initial commit

--- cpp00_string_manipulation/ex02/Account.cpp
+++ cpp00_string_manipulation/ex02/Account.cpp
@@ -1,1 +1,29 @@
 #include "Account.hpp"
+#include <iostream>
+#include <chrono> 
+#include <ctime> 
+
+int Account::_nbAccounts = 0;
+int Account::_totalAmount = 0;
+int Account::_totalNbDeposits = 0;
+int Account::_totalNbWithdrawals = 0;
+
+void	Account::_displayTimestamp( void )
+{
+    time_t now;
+    std::time(&now);
+    std::cout <<  now << " ";
+}
+
+Account::Account( int initial_deposit )
+{
+    _nbAccounts += 1;
+	_totalAmount += initial_deposit;
+	_totalNbDeposits += 1;
+	_accountIndex = _nbAccounts - 1;
+	_amount = initial_deposit;
+	_nbDeposits = 1;
+	_nbWithdrawals = 0;
+    _displayTimestamp();
+    std::cout << "index:" << _accountIndex << ";amount:" << _amount << ";created" << std::endl;
+}
