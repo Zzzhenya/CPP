b43baff4e34052ae99003742a9340a4220272003
Shenya De Silva <shenya44@outlook.com>
2025-04-03 21:01:52 +0200

    Cleaned the manual tests

--- cpp06/ex00/main.cpp
+++ cpp06/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: sde-silv <sde-silv@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/06 01:19:39 by sde-silv          #+#    #+#             */
-/*   Updated: 2024/09/12 15:05:34 by sde-silv         ###   ########.fr       */
+/*   Updated: 2025/04/03 21:01:34 by sde-silv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,48 +35,13 @@ void	run_converter(const char *ptr)
 int main(int argc, char **argv)
 {
 	if (argc == 2)
+	{
 		run_converter(argv[1]);
+	}
 	else
 	{
 		std::cout << "Usage: ./converter <value>" << std::endl;
 		std::cout << "Usage(debug): ./dbg_converter <value>" << std::endl;
 	}
-// #ifdef _DEBUG
-	{
-		int a = 120;
-		// char *val = NULL;
-		// run_converter("A");
-		// run_converter("");
-		// run_converter(NULL);
-		// run_converter(val);
-		// run_converter("ABC");
-		// run_converter("0");
-		// run_converter("12");
-		// run_converter("34");
-		// run_converter("nan");
-		// run_converter("-inf");
-		// run_converter("inf");
-		// run_converter("+inf");
-		// run_converter("NULL");
-		// run_converter(" ");
-		try {
-			run_converter((char *)&a);
-			run_converter((char *)120);
-		}
-		catch (std::exception &e){
-			std::cout << e.what() << std::endl;
-		}
-	}
-// #endif
-	// {
-	// 	// std::cout << std::numeric_limits<double>::max() << std::endl;
-	// 	std::cout << std::numeric_limits<double>::min() << std::endl;
-	// 	// std::cout << std::numeric_limits<float>::max() << std::endl;
-	// 	// std::cout << std::numeric_limits<float>::min() << std::endl;
-	// 	ScalarConverter::convert(argv[1]);
-	// 	// std::cout << static_cast <float> (std::stof(argv[1])) << std::endl;
-	// 	std::cout << std::numeric_limits<float>::max() << std::endl;
-	// 	std::cout << std::numeric_limits<float>::min() << std::endl;
-	// }
 	return (0);
 }
