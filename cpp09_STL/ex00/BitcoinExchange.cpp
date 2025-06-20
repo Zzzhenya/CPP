2df4f7f647f1aa83d2aa23ed7c4ce942683c859f
Shenya De Silva <shenya44@outlook.com>
2025-06-20 21:25:15 +0200

    test: feature check before leaks

--- cpp09_STL/ex00/BitcoinExchange.cpp
+++ cpp09_STL/ex00/BitcoinExchange.cpp
@@ -10,126 +10,6 @@ BitcoinExchange::~BitcoinExchange(void)
 	debug("BitcoinExchange default destructor");
 }
 
-// Because the stringstream parsing for date only checks for numbers;
-// and not the leading zeros and spaces
-std::string getDate(std::string date)
-{
-	int year;
-	int month;
-	int day;
-	char sep1;
-	char sep2;
-
-	std::istringstream ss(date);
-
-	ss>>year;
-	ss>>sep1;
-	ss>>month;
-	ss>>sep2;
-	ss>>day;
-
-	std::stringstream ret;
-	ret << year << sep1;
-	if (month < 10)
-		ret << 0 << month;
-	else
-		ret << month;
-	ret << sep2;
-	if (day < 10)
-		ret << 0 << day;
-	else
-		ret << day;
-	return (ret.str());
-}
-
-int BitcoinExchange::read_csv_lines(std::fstream &db)
-{
-	std::string				line;
-	std::string::size_type	ret;
-	int row = 0;
-	// int errcount = 0;
-
-	debug("read_csv_lines");
-	while (std::getline(db, line))
-	{
-		// When a row is empty
-		if (line.empty())
-			return (row);
-		ret = line.find(",");
-		// When there is no commas
-		if (ret == std::string::npos)
-			return (row);
-		// std::cout <<row  <<": "<< line << std::endl;
-		// If comma is not at the end of the row -> process
-		if (ret + 1 != std::string::npos)
-		{
-			std::string first = line.substr(0, ret);
-			std::string second = line.substr(ret + 1, line.length() - 1);
-			// std::cout <<row  <<": "<< first << " " << second << std::endl;
-			// When there is only one comma, and strings are not empty -> insert to map
-			if (first.find(",") != std::string::npos || second.find(",") != std::string::npos)
-				return (row);
-			else if (first.empty() || second.empty())
-				return (row);
-			else
-			{
-				if (row > 0)
-				{
-					//dbmap.insert(std::pair<std::string,std::string>(first,second));
-					std::pair<std::map<std::string,std::string>::iterator, bool> res = dbmap.insert(std::pair<std::string,std::string>(getDate(first),second));
-					if (res.second != true)
-					{
-						warning("insertion failed for row: " << row << " => " << line );
-					}
-				}
-			}
-		}
-		else
-			return (row);
-		row++;
-	}
-	// When file is empty
-	if (dbmap.empty())
-		return (-1);
-	return (0);
-}
-
-bool BitcoinExchange::initDB(void)
-{
-	debug("initDB");
-	try
-	{
-		debug("openFile");
-		std::fstream db(DATABASE_NAME, std::ios::in);
-		if (!db.is_open() || db.bad() || db.fail())
-		{
-			if (!db.is_open())
-				message("File open error" << DATABASE_NAME);
-			if (db.bad())
-				message("badbit is set: " << DATABASE_NAME);
-			if (db.fail())
-				message(std::strerror(errno));
-			return (false);
-		}
-		debug("closeFile");
-		int line  = read_csv_lines(db);
-		if (line != 0)
-		{
-			message("CSV parse error on line: " << line);
-			dbmap.clear();
-		}
-		db.close();
-		if (!validateDB())
-			return (false);
-		return (true);
-	}
-	catch (const std::exception &e)
-	{
-		std::cout << e.what() << DATABASE_NAME << std::endl;
-	}
-	return (false);
-}
-
 bool validateDate(std::string date)
 {
 	int year;
@@ -144,7 +24,14 @@ bool validateDate(std::string date)
 
 	if (!(ss>>year)) return (false);
 
-	if (year < 2000 ) return (false); //|| year > current year
+	if (year < 2000 ) return (false); 
+
+	//|| year > current year
+	// std::time_t result = std::time(NULL);
+	// std::tm *tm = localtime(&result);
+
+	// if (year > tm->tm_year + 1900) return (false);
+
 	if (!(ss>>sep1)) return (false);
 	if (sep1 != '-') return (false);
 
@@ -159,6 +46,21 @@ bool validateDate(std::string date)
 	if (!(ss>>day)) return (false);
 
 	if (day < 1 || day > 31) return (false);
+
+	if (month == 4 || month == 6 || month == 9 || month == 11 )
+	{
+		if (day > 30) return (false);
+	}
+
+	if (month == 2)
+	{
+		bool leapyear = false;
+		leapyear =  (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+
+		if (leapyear && day > 29) return (false);
+
+		if (!leapyear && day > 28) return (false);
+	}
 
 	return (true);
 }
@@ -231,27 +133,156 @@ bool validateRate(std::string val)
 	return (true);
 }
 
-bool BitcoinExchange::validateDB(void)
+// Because the stringstream parsing for date only checks for numbers;
+// and not the leading zeros and spaces
+// Isn't really needed when validating before data insertion
+std::string getDate(std::string date)
 {
-	int line = 1;
-	if (dbmap.empty())
-		return (false);
-	for (std::map<std::string,std::string>::iterator it = dbmap.begin(); it != dbmap.end(); it++)
+	int year;
+	int month;
+	int day;
+	char sep1;
+	char sep2;
+
+	std::istringstream ss(date);
+
+	ss>>year;
+	ss>>sep1;
+	ss>>month;
+	ss>>sep2;
+	ss>>day;
+
+	std::stringstream ret;
+	ret << year << sep1;
+	if (month < 10)
+		ret << 0 << month;
+	else
+		ret << month;
+	ret << sep2;
+	if (day < 10)
+		ret << 0 << day;
+	else
+		ret << day;
+	return (ret.str());
+}
+
+int BitcoinExchange::read_csv_lines(std::fstream &db)
+{
+	std::string				line;
+	std::string::size_type	ret;
+	int row = 0;
+	// int errcount = 0;
+
+	debug("read_csv_lines");
+	while (std::getline(db, line))
 	{
-		bool date = 0;
-		bool val = 0;
-		date = validateDate(it->first);
-		val = validateRate(it->second);
-		if (!date)
-			message("Invalid date on: " << line <<" :"<< it->first);
-		if (!val)
-			message("Invalid value on: " << line <<" :"<< it->second);
-		if (!date || !val)
+		// When a row is empty
+		if (line.empty())
+			return (row + 1);
+		ret = line.find(",");
+		// When there is no commas
+		if (ret == std::string::npos)
+			return (row + 1);
+		// std::cout <<row  <<": "<< line << std::endl;
+		// If comma is not at the end of the row -> process
+		if (ret + 1 != std::string::npos)
 		{
-			dbmap.clear();
+			std::string first = line.substr(0, ret);
+			std::string second = line.substr(ret + 1, line.length() - 1);
+			// std::cout <<row  <<": "<< first << " " << second << std::endl;
+			// When there is only one comma, and strings are not empty -> insert to map
+			if (first.find(",") != std::string::npos || second.find(",") != std::string::npos)
+				return (row + 1);
+			else if (first.empty() || second.empty())
+				return (row + 1);
+			else
+			{
+				if (row > 0)
+				{
+					//dbmap.insert(std::pair<std::string,std::string>(first,second));
+					if (validateDate(first) && validateRate(second))
+					{
+						std::pair<std::map<std::string,std::string>::iterator, bool> res = dbmap.insert(std::pair<std::string,std::string>(getDate(first),second));
+						if (res.second != true)
+						{
+							warning("insertion failed for row: " << row << " => " << line );
+						}
+					}
+					else
+						return (row + 1);
+				}
+			}
+		}
+		else
+			return (row + 1);
+		row++;
+	}
+	// When file is empty
+	if (dbmap.empty())
+		return (-1);
+	return (0);
+}
+
+bool BitcoinExchange::initDB(void)
+{
+	debug("initDB");
+	try
+	{
+		debug("openFile");
+		std::fstream db(DATABASE_NAME, std::ios::in);
+		if (!db.is_open() || db.bad() || db.fail())
+		{
+			if (!db.is_open())
+				message("File open error" << DATABASE_NAME);
+			if (db.bad())
+				message("badbit is set: " << DATABASE_NAME);
+			if (db.fail())
+				message(std::strerror(errno));
 			return (false);
 		}
+		debug("closeFile");
+		int line  = read_csv_lines(db);
+		if (line != 0)
+		{
+			message("CSV parse error on line: " << line);
+			dbmap.clear();
+		}
+		db.close();
+		// db validation
+		if (!validateDB())
+			return (false);
+		return (true);
 	}
+	catch (const std::exception &e)
+	{
+		std::cout << e.what() << DATABASE_NAME << std::endl;
+	}
+	return (false);
+}
+
+bool BitcoinExchange::validateDB(void)
+{
+	// int line = 1;
+	if (dbmap.empty())
+		return (false);
+	// post db validation removed
+	// for (std::map<std::string,std::string>::iterator it = dbmap.begin(); it != dbmap.end(); it++)
+	// {
+	// 	bool date = 0;
+	// 	bool val = 0;
+	// 	
+	// 	date = validateDate(it->first);
+	// 	val = validateRate(it->second);
+	// 	if (!date)
+	// 		message("Invalid date on: " << line <<" :"<< it->first);
+	// 	if (!val)
+	// 		message("Invalid value on: " << line <<" :"<< it->second);
+	// 	if (!date || !val)
+	// 	{
+	// 		dbmap.clear();
+	// 		return (false);
+	// 	}
+	// }
 	return (true);
 }
 
@@ -298,18 +329,30 @@ bool BitcoinExchange::validateFile(char *str)
 	return (false);
 }
 
+/*
+ outofrange true when date < database.min() || date > database.max()
+*/
 void BitcoinExchange::Query(std::string date, std::string amount)
 {
 	std::map<std::string,std::string>::iterator it = dbmap.find(date);
+	bool outofrange = false;
 	if (it == dbmap.end())
 	{
 		it = dbmap.lower_bound(date);
-		it --;
+		if (it != dbmap.end() && it != dbmap.begin())
+			it --;
+		else
+			outofrange = true;
 	}
 	//debug(it->second);
-	std::cout << date << "\t:" << amount << "\t* " \
-	<< it->second << "\t=\t"\
-	<<  (std::atof(amount.c_str())) * (std::atof(it->second.c_str())) << std::endl;
+	if (!outofrange)
+	{
+		std::cout << date << "\t:" << amount << "\t* " \
+		<< it->second << "\t=\t"\
+		<<  (std::atof(amount.c_str())) * (std::atof(it->second.c_str())) << std::endl;
+	}
+	else
+		message(date << " is out of range for the db");
 }
 
 void BitcoinExchange::runQuery(int no, std::string str)
@@ -331,7 +374,9 @@ void BitcoinExchange::runQuery(int no, std::string str)
 			bool date = validateDate(first);
 			bool amount = validateAmount(second);
 			if (!date)
-				message("bad input => " << first);
+				message("invalid date => " << first);
+			// if (!amount)
+			// 	message("invalid amount => " << second);
 			if (date && amount)
 			{
 				Query(getDate(first), second);
@@ -339,7 +384,7 @@ void BitcoinExchange::runQuery(int no, std::string str)
 			}
 		}
 		else
-			message("wrong format");
+			message("wrong format => " << str);
 	}
 	// std::istringstream iss(str);
 
